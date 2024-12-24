6d787b6701c725a132bb0a94bb59b25f46dbe967
LucaM <luca.migliaccio4@studenti.unina.it>
2024-12-24 10:32:01 +0100

    Test della creazione di un pagamento tra due wallet su XRPL, settando i parametri. L'output di questo test è la response ottenuta a seguito del trasferimento.

--- test_func.py
+++ test_func.py
@@ -2,7 +2,10 @@ from xrpl.core import keypairs
 from xrpl.wallet import Wallet, generate_faucet_wallet
 from xrpl.clients import JsonRpcClient
 from xrpl.account import get_balance
-from xrpl.clients import JsonRpcClient
+from xrpl.models import Payment
+from xrpl.transaction import sign, submit_and_wait
+from xrpl.account import get_next_valid_seq_number
+from xrpl.ledger import get_latest_validated_ledger_sequence
 
 # Test di generazione di un seed
 seed = keypairs.generate_seed()
@@ -25,3 +28,29 @@ print(f"Wallet seed: {wallet.seed}")
 address = wallet.address
 balance = get_balance(address, client)
 print(f"Balance for {address}: {balance}")
+
+#######################################################
+# TEST DELLA CREAZIONE DI UNA TRANSAZIONE DI PAGAMENTO TRA DUE WALLET #
+#######################################################
+
+# Creazione di due wallet finanziati
+sender_wallet = generate_faucet_wallet(client, debug=True)
+receiver_wallet = generate_faucet_wallet(client, debug=True)
+
+# Creazione della transazione di pagamento
+current_validated_ledger = get_latest_validated_ledger_sequence(client)
+payment_tx = Payment(
+    account=sender_wallet.classic_address,
+    amount="1000",  # 1000 drops di XRP (1 XRP = 1,000,000 drops)
+    destination=receiver_wallet.classic_address,
+    sequence=get_next_valid_seq_number(sender_wallet.classic_address, client),
+    last_ledger_sequence=current_validated_ledger + 10,  # Valido per i prossimi 10 ledger
+    fee="10",  # Fee minima in drops (tassa che dev'essere pagata da chi effettua la transazione)
+)
+
+# Firma e invio della transazione
+signed_tx = sign(payment_tx, sender_wallet)
+response = submit_and_wait(signed_tx, client)
+
+# Output
+print(f"Payment transaction response: {response}")
