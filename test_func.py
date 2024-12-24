67ceb33ad0f0b4e9af45e906f4175dee2a56aa1d
LucaM <luca.migliaccio4@studenti.unina.it>
2024-12-24 09:55:34 +0100

    Test libreria Wallet: creazione di un wallet a partire da un seed.

--- test_func.py
+++ test_func.py
@@ -1,5 +1,10 @@
 from xrpl.core import keypairs
+from xrpl.wallet import Wallet
 
 # Test di generazione di un seed
 seed = keypairs.generate_seed()
 print(f"Generated seed: {seed}")
+
+# Test per creare un wallet da un seed
+wallet = Wallet.from_seed(seed)
+print(f"Wallet address: {wallet.address}")
