57c4c794503047784e875dd6e1ce8f979245314e
LucaM <luca.migliaccio4@studenti.unina.it>
2024-12-24 09:57:36 +0100

    Test libreria JsonRpcClient: connessione alla testnet di XRPL.

--- test_func.py
+++ test_func.py
@@ -1,5 +1,6 @@
 from xrpl.core import keypairs
 from xrpl.wallet import Wallet
+from xrpl.clients import JsonRpcClient
 
 # Test di generazione di un seed
 seed = keypairs.generate_seed()
@@ -8,3 +9,7 @@ print(f"Generated seed: {seed}")
 # Test per creare un wallet da un seed
 wallet = Wallet.from_seed(seed)
 print(f"Wallet address: {wallet.address}")
+
+# Test della connessione al client XRPL
+client = JsonRpcClient("https://s.altnet.rippletest.net:51234")
+print(f"Client connected to: {client.url}")
