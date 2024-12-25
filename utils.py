bbbd0317b37e506f35645f4b07a4690149bf7038
LucaM <luca.migliaccio4@studenti.unina.it>
2024-12-25 12:10:28 +0100

    Import di tutte le librerie necessarie.

---
+++ utils.py
@@ -0,0 +1,11 @@
+from xrpl.account import get_balance, get_next_valid_seq_number
+from xrpl.clients import JsonRpcClient
+from xrpl.wallet import Wallet, generate_faucet_wallet
+from xrpl.core import keypairs
+from xrpl.transaction import sign, submit_and_wait, XRPLReliableSubmissionException
+from xrpl.models import Payment
+from xrpl.models.transactions import NFTokenMint, NFTokenCreateOffer, NFTokenAcceptOffer
+from xrpl.utils import str_to_hex, datetime_to_ripple_time
+from xrpl.ledger import get_latest_validated_ledger_sequence
+from datetime import datetime, timedelta
+
