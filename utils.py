0ddf3690b2942f19bc1157877a5b954ce0e97202
LucaM <luca.migliaccio4@studenti.unina.it>
2024-12-25 12:12:28 +0100

    Aggiunta funzione _generate_xrpl_wallet_seed per generare un seed casuale per wallet XRPL. La funzione consente di generare un seed casuale che può essere utilizzato per creare un wallet XRPL. Viene specificato che il wallet generato non è utilizzabile senza finanziamento.

--- utils.py
+++ utils.py
@@ -9,3 +9,11 @@ from xrpl.utils import str_to_hex, datetime_to_ripple_time
 from xrpl.ledger import get_latest_validated_ledger_sequence
 from datetime import datetime, timedelta
 
+client = JsonRpcClient("https://s.altnet.rippletest.net:51234")
+
+def _generate_xrpl_wallet_seed() -> str:
+    """
+    Generate a random seed. Notice that if you create a wallet with this seed, it won't be a working wallet, because you need to fund it before. See https://xrpl.org/docs/concepts/accounts#creating-accounts.
+    """
+    return keypairs.generate_seed()
+
